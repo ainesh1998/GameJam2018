2b806f47cb09a7fac9591a3ce8b16a1d8d524429
Ainesh Sevellaraja <ainesh1998@outlook.com>
2018-11-20 07:05:01 +0000

    done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     bool gameHasEnded = false;
     public float delay;
+    public player player;
+    public Text winText;
+    public int level;
+
+    private void Start()
+    {
+       // level = 1;
+       // DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) SceneManager.LoadScene("Menu");
+        if (Input.GetKeyDown(KeyCode.R)) restart();
+    }
+
+    public void levelWin()
+    {
+        player.completeLevelUI.SetActive(true);
+        player.enabled = false;
+        Destroy(player.GetComponent<Rigidbody>());
+        //open new level
+        level += 1;
+        Invoke("loadNextLevel", delay);
+
+    }
+
+    public void loadNextLevel(){
+        SceneManager.LoadScene("Level" + level.ToString(), LoadSceneMode.Single);
+    }
 
     public void gameWin() {
-        if (gameHasEnded == false) {
+        if (gameHasEnded == false)
+        {
             gameHasEnded = true;
-            Debug.Log("You have won!");
+            player.completeLevelUI.SetActive(true);
+            winText.text = "You have won!";
+            player.enabled = false;
+            Destroy(player.GetComponent<Rigidbody>());
+            Invoke("backToMenu", delay);
         }
 
     }
@@ -19,15 +55,15 @@ public class GameManager : MonoBehaviour {
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
-            Debug.Log("You have lost.");
-            //restart();
-            Debug.Log("Respawning");
-            Invoke("restart",delay);
+            restart();
         }
-
     }
 
     void restart() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void backToMenu() {
+        SceneManager.LoadScene("Menu");
     }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
+public class player : MonoBehaviour
+{
 
     Rigidbody rb;
     public float xForce;
     //public float maxVeloc;
     public float thrust;
     private bool isGrounded;
-    //public GameManager gameManager;
+    public GameManager gameManager;
+    public GameObject completeLevelUI;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         isGrounded = true;
+        gameManager = FindObjectOfType<GameManager>();
+        completeLevelUI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,7 +32,7 @@ public class player : MonoBehaviour {
     {
         float x = Input.GetAxis("Horizontal");
         //float yVeloc = rb.velocity.y;
-        bool isJump = Input.GetButtonDown("Jump");
+        bool isJump = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
         Vector3 jump = new Vector3(0, 100, 0);
 
         if (x > 0)
@@ -44,8 +48,9 @@ public class player : MonoBehaviour {
             isGrounded = false;
         }
 
-        if (rb.position.y < -3f) {
-            FindObjectOfType<GameManager>().gameLost();
+        if (rb.position.y < -15f)
+        {
+            gameManager.gameLost();
         }
 
     }
@@ -53,14 +58,28 @@ public class player : MonoBehaviour {
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground")) isGrounded = true;
+
+        if (collision.gameObject.CompareTag("Spike")) gameManager.gameLost();
+
+        if (collision.gameObject.CompareTag("Breakable")) {
+            collision.gameObject.AddComponent<Rigidbody>();
+            collision.gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("LevelWin"))
+        {
+            gameManager.levelWin();
+        }
+
         if (other.CompareTag("GameWin"))
         {
-            FindObjectOfType<GameManager>().gameWin();
+            gameManager.gameWin();
         }
+
+
     }
 
 }
